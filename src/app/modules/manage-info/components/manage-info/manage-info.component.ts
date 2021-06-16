0cb21b0aa32d8242fbe6fa0e4a7512670c8528b2
ashish.jad <ashish.jad>
2021-06-16 19:08:54 +0530

    table column name change vaccinated to vaccinated1

--- src/app/modules/manage-info/components/manage-info/manage-info.component.ts
+++ src/app/modules/manage-info/components/manage-info/manage-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Renderer2,ViewChild,ElementRef } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { UrlService } from 'src/app/shared/services/url.service';
 import { Title } from '@angular/platform-browser';
 import { RouteguardService } from 'src/app/shared/services/routeguard.service';
@@ -7,110 +7,110 @@ import { AppLevelDataPassService } from 'src/app/shared/services/app-level-data-
 // import { DatePickerModel } from '@syncfusion/ej2-calendars';
 import { Router, Routes, ActivatedRoute } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import {ManageInfoService} from "../../services/manage-info.service";
+import { ManageInfoService } from "../../services/manage-info.service";
 import { NgIf, JsonPipe } from '@angular/common';
- 
- var getAssociatesExtensionsAlldata= {}; 
+
+var getAssociatesExtensionsAlldata = {};
 @Component({
   selector: 'app-manage-info',
   templateUrl: './manage-info.component.html',
   styleUrls: ['./manage-info.component.scss']
 })
- 
+
 export class ManageInfoComponent implements OnInit {
-  
-//  dropDownvalueSelector={
-//     confirmedCase:0,
-//     activeCase:0,
-//     recoveredCase:0,
-//     deceasedCase:0,
-//     NewConfirmed:0,
-//     NewDeaths:0,
-//     NewRecovered:0,
-//     countrySelector:"",
-//     selectedCountry:"India"
-//  }; 
- stateISDname={  
-  "AN":"Andaman and Nicobar Islands",
-  "AP":"Andhra Pradesh",
-  "AR":"Arunachal Pradesh",
-  "AS":"Assam",
-  "BR":"Bihar",
-  "CH":"Chandigarh",
-  "CT":"Chhattisgarh",
-  "DN":"Dadra and Nagar Haveli",
-  "DD":"Daman and Diu",
-  "DL":"Delhi",
-  "GA":"Goa",
-  "GJ":"Gujarat",
-  "HR":"Haryana",
-  "HP":"Himachal Pradesh",
-  "JK":"Jammu and Kashmir",
-  "JH":"Jharkhand",
-  "KA":"Karnataka",
-  "KL":"Kerala",
-  "LA":"Ladakh",
-  "LD":"Lakshadweep",
-  "MP":"Madhya Pradesh",
-  "MH":"Maharashtra",
-  "MN":"Manipur",
-  "ML":"Meghalaya",
-  "MZ":"Mizoram",
-  "NL":"Nagaland",
-  "OR":"Odisha",
-  "PY":"Puducherry",
-  "PB":"Punjab",
-  "RJ":"Rajasthan",
-  "SK":"Sikkim",
-  "TN":"Tamil Nadu",
-  "TG":"Telangana",
-  "TR":"Tripura",
-  "UP":"Uttar Pradesh",
-  "UT":"Uttarakhand",
-  "WB":"West Bengal",
-  "TT":"thiruvananthapuram"
-}
- districtData;
- updatelognews=[]
- goSlideDown= false;
- toggle
-//  totalCountryCount={
-//   TotalConfirmed:0,
-//   TotalRecovered:0,
-//   TotalDeaths:0
-//  }
-modalObject={
-  associatedStateDataTable:false
-}
-associatedStateCategoryData=[];
-  countryNames=[];
-  totalCountryCount=[]
-  constructor(     
+
+  //  dropDownvalueSelector={
+  //     confirmedCase:0,
+  //     activeCase:0,
+  //     recoveredCase:0,
+  //     deceasedCase:0,
+  //     NewConfirmed:0,
+  //     NewDeaths:0,
+  //     NewRecovered:0,
+  //     countrySelector:"",
+  //     selectedCountry:"India"
+  //  }; 
+  stateISDname = {
+    "AN": "Andaman and Nicobar Islands",
+    "AP": "Andhra Pradesh",
+    "AR": "Arunachal Pradesh",
+    "AS": "Assam",
+    "BR": "Bihar",
+    "CH": "Chandigarh",
+    "CT": "Chhattisgarh",
+    "DN": "Dadra and Nagar Haveli",
+    "DD": "Daman and Diu",
+    "DL": "Delhi",
+    "GA": "Goa",
+    "GJ": "Gujarat",
+    "HR": "Haryana",
+    "HP": "Himachal Pradesh",
+    "JK": "Jammu and Kashmir",
+    "JH": "Jharkhand",
+    "KA": "Karnataka",
+    "KL": "Kerala",
+    "LA": "Ladakh",
+    "LD": "Lakshadweep",
+    "MP": "Madhya Pradesh",
+    "MH": "Maharashtra",
+    "MN": "Manipur",
+    "ML": "Meghalaya",
+    "MZ": "Mizoram",
+    "NL": "Nagaland",
+    "OR": "Odisha",
+    "PY": "Puducherry",
+    "PB": "Punjab",
+    "RJ": "Rajasthan",
+    "SK": "Sikkim",
+    "TN": "Tamil Nadu",
+    "TG": "Telangana",
+    "TR": "Tripura",
+    "UP": "Uttar Pradesh",
+    "UT": "Uttarakhand",
+    "WB": "West Bengal",
+    "TT": "thiruvananthapuram"
+  }
+  districtData;
+  updatelognews = []
+  goSlideDown = false;
+  toggle
+  //  totalCountryCount={
+  //   TotalConfirmed:0,
+  //   TotalRecovered:0,
+  //   TotalDeaths:0
+  //  }
+  modalObject = {
+    associatedStateDataTable: false
+  }
+  associatedStateCategoryData = [];
+  countryNames = [];
+  totalCountryCount = []
+  constructor(
     private urlService: UrlService,
     private titleService: Title,
     private routeService: RouteguardService,
     private router: Router,
     private appService: AppLevelDataPassService,
-    private ManageInfoService:ManageInfoService,
-    private renderer:Renderer2,
+    private ManageInfoService: ManageInfoService,
+    private renderer: Renderer2,
     private route: ActivatedRoute,
     // private DatePickerModel:DatePickerModel
-  ) {}
-  getAssociatesExtensionsAlldata={}
+  ) { }
+  getAssociatesExtensionsAlldata = {}
 
   ngOnInit() {
-    this.appService.setRouterLoader(false);   
-        this.titleService.setTitle('manage-info')         
-        this.initFunction();
+    this.appService.setRouterLoader(false);
+    this.titleService.setTitle('manage-info')
+    this.initFunction();
   }
-  
-  initFunction(){    
+
+  initFunction() {
     // this.getManageAccounts(); 
     // this.getmanageCountryInfo(this.dropDownvalueSelector.selectedCountry)
-     this.Loginfo();
+    this.Loginfo();
     this.getStateTableInfo();
     //this.onloadAnimMap();   
-   // this.getmanageCalendarDate("2020-09-17");  
+    // this.getmanageCalendarDate("2020-09-17");  
   }
   // getmanageCountryInfo(selectedValue){   
   //   this.ManageInfoService.getmanageCountrynameInfo().subscribe(val=>{
@@ -130,42 +130,42 @@ associatedStateCategoryData=[];
   //     }
   //   });
   // }
-  Loginfo(){
-    this.ManageInfoService.getLogInfo().subscribe(val=>{   
-        Object.keys(val).forEach((objectKey,value) => {
-          // var splitted = str.replace(',', '<br/>')
-          //console.log(val[objectKey])
-              this.updatelognews.push(val[objectKey].update.replace(',','\n'));
-              //console.log(val[objectKey].update.split(','))
-            //this.updatelognews.push("1282 new cases /n 1418 recoveries and 9 deaths in Madhya Pradesh")
-            
-      });   
-    });    
-  }
-  stateTableData=[];
-  getStateTableInfo(){
-    this.ManageInfoService.getStateInfo().subscribe(val=>{ 
-      //console.log(val)
-      this.districtData=val
-        Object.keys(val).forEach((objectKey,value) => { 
-         // console.log(val[objectKey])
-          this.stateTableData.push(            
-            [ objectKey,
-              val[objectKey].total.confirmed.toLocaleString('en-GB'),
-              (val[objectKey].total.confirmed-val[objectKey].total.recovered).toLocaleString('en-GB'),
-              val[objectKey].total.recovered.toLocaleString('en-GB'),
-              val[objectKey].total.deceased.toLocaleString('en-GB'),
-              val[objectKey].total.tested.toLocaleString('en-GB'),
-              val[objectKey].total.vaccinated.toLocaleString('en-GB')
-            ]            
-          );
-      });
-      this.enter('MH','Maharashtra');
+  Loginfo() {
+    this.ManageInfoService.getLogInfo().subscribe(val => {
+      Object.keys(val).forEach((objectKey, value) => {
+        // var splitted = str.replace(',', '<br/>')
+        //console.log(val[objectKey])
+        this.updatelognews.push(val[objectKey].update.replace(',', '\n'));
+        //console.log(val[objectKey].update.split(','))
+        //this.updatelognews.push("1282 new cases /n 1418 recoveries and 9 deaths in Madhya Pradesh")
 
-    }); 
+      });
+    });
   }
- 
-  
+  stateTableData = [];
+  getStateTableInfo() {
+    this.ManageInfoService.getStateInfo().subscribe(val => {
+      //console.log(val)
+      this.districtData = val
+      Object.keys(val).forEach((objectKey, value) => {
+        console.log(val[objectKey])
+        this.stateTableData.push(
+          [objectKey,
+            val[objectKey].total.confirmed.toLocaleString('en-GB'),
+            (val[objectKey].total.confirmed - val[objectKey].total.recovered).toLocaleString('en-GB'),
+            val[objectKey].total.recovered.toLocaleString('en-GB'),
+            val[objectKey].total.deceased.toLocaleString('en-GB'),
+            val[objectKey].total.tested.toLocaleString('en-GB'),
+            val[objectKey].total.vaccinated1.toLocaleString('en-GB')
+          ]
+        );
+      });
+      this.enter('MH', 'Maharashtra');
+
+    });
+  }
+
+
   // getmanageCalendarDate(date){
   //   console.log(date);
 
@@ -174,107 +174,107 @@ associatedStateCategoryData=[];
   //       console.log(val);
   //     });
   // }
-  districtTableData=[];
-  selectedValueTitle;population;  
-  deltaValue=[];
+  districtTableData = [];
+  selectedValueTitle; population;
+  deltaValue = [];
   //selected value of state
-  selectedStateAttr=[];
-  districtDatashow(selectedValue){
-    this.selectedStateAttr=[]
-    this.districtTableData=[];
-    this.selectedValueTitle="";
+  selectedStateAttr = [];
+  districtDatashow(selectedValue) {
+    this.selectedStateAttr = []
+    this.districtTableData = [];
+    this.selectedValueTitle = "";
     this.selectedStateAttr.push(selectedValue)
-      this.selectedValueTitle="Of " +this.stateISDname[selectedValue];
-   // console.log(selectedValue)
-    this.modalObject['associatedStateDataTable'] = true;  
-    
-    Object.keys(this.districtData).forEach((objectKey,value) => {
-      if(objectKey==selectedValue){
-        this.population= this.districtData[selectedValue].meta.population
-        if(this.districtData[selectedValue].districts !== undefined){
-          Object.keys(this.districtData[selectedValue].districts).forEach((objectKey,value) => {  
-            this.districtTableData.push(            
-              [ objectKey,
+    this.selectedValueTitle = "Of " + this.stateISDname[selectedValue];
+    // console.log(selectedValue)
+    this.modalObject['associatedStateDataTable'] = true;
+
+    Object.keys(this.districtData).forEach((objectKey, value) => {
+      if (objectKey == selectedValue) {
+        this.population = this.districtData[selectedValue].meta.population
+        if (this.districtData[selectedValue].districts !== undefined) {
+          Object.keys(this.districtData[selectedValue].districts).forEach((objectKey, value) => {
+            this.districtTableData.push(
+              [objectKey,
                 this.districtData[selectedValue].districts[objectKey].total.confirmed,
-                this.districtData[selectedValue].districts[objectKey].total.confirmed-(this.districtData[selectedValue].districts[objectKey].total.recovered + this.districtData[selectedValue].districts[objectKey].total.deceased),
+                this.districtData[selectedValue].districts[objectKey].total.confirmed - (this.districtData[selectedValue].districts[objectKey].total.recovered + this.districtData[selectedValue].districts[objectKey].total.deceased),
                 this.districtData[selectedValue].districts[objectKey].total.recovered,
                 this.districtData[selectedValue].districts[objectKey].total.deceased,
                 this.districtData[selectedValue].districts[objectKey].total.tested,
-                this.districtData[selectedValue].districts[objectKey].total.vaccinated,
-              ]             
+                this.districtData[selectedValue].districts[objectKey].total.vaccinated1,
+              ]
             );
           });
-        }        
-        this.districtTableData.forEach(function(value,index){
-          for(var i = 1;i<value.length;i++){
-            if(value[i] == undefined || value[i] == " "){
+        }
+        this.districtTableData.forEach(function (value, index) {
+          for (var i = 1; i < value.length; i++) {
+            if (value[i] == undefined || value[i] == " ") {
               value[i] = "-";
-            }else if( isNaN(value[i])){
+            } else if (isNaN(value[i])) {
               value[i] = 0;
             }
           }
         });
-        this.deltaValue=[];
-        if(this.districtData[selectedValue].delta  !== undefined)
-        Object.keys(this.districtData[selectedValue].delta).forEach((deltaKeys,index) => { 
-          this.deltaValue.push(
-            [deltaKeys,this.districtData[selectedValue].delta[deltaKeys]]
-          )
-        });
+        this.deltaValue = [];
+        if (this.districtData[selectedValue].delta !== undefined)
+          Object.keys(this.districtData[selectedValue].delta).forEach((deltaKeys, index) => {
+            this.deltaValue.push(
+              [deltaKeys, this.districtData[selectedValue].delta[deltaKeys]]
+            )
+          });
       }
-    });    
+    });
   }
-  GoDown(){
-    if(this.goSlideDown==true){
-      this.goSlideDown=false;
-    }else{
-      this.goSlideDown=true;
+  GoDown() {
+    if (this.goSlideDown == true) {
+      this.goSlideDown = false;
+    } else {
+      this.goSlideDown = true;
     }
   }
   maploadtimer
-  hoverStateSelector={
-    confirmedCase:0,
-    activeCase:0,
-    recoveredCase:0,
-    deceasedCase:0,  
-    testedcase:0,
-    vaccinatcase:0,  
-    selectedState:"MH"
- };
-  enter(stateattr,value) {   
-    this.hoverStateSelector.selectedState=value;
-    this.stateTableData.forEach(element => {      
-      if(element[0]==stateattr){
-        this.hoverStateSelector.confirmedCase=element[1];
-        this.hoverStateSelector.activeCase=element[2];
-        this.hoverStateSelector.recoveredCase=element[3];
-        this.hoverStateSelector.deceasedCase=element[4];
-        this.hoverStateSelector.testedcase=element[5];
-        this.hoverStateSelector.vaccinatcase=element[6];
-      }     
-    });   
+  hoverStateSelector = {
+    confirmedCase: 0,
+    activeCase: 0,
+    recoveredCase: 0,
+    deceasedCase: 0,
+    testedcase: 0,
+    vaccinatcase: 0,
+    selectedState: "MH"
+  };
+  enter(stateattr, value) {
+    this.hoverStateSelector.selectedState = value;
+    this.stateTableData.forEach(element => {
+      if (element[0] == stateattr) {
+        this.hoverStateSelector.confirmedCase = element[1];
+        this.hoverStateSelector.activeCase = element[2];
+        this.hoverStateSelector.recoveredCase = element[3];
+        this.hoverStateSelector.deceasedCase = element[4];
+        this.hoverStateSelector.testedcase = element[5];
+        this.hoverStateSelector.vaccinatcase = element[6];
+      }
+    });
     // if(document.querySelectorAll(".fillcolor").length >0){
     //   document.querySelector(".fillcolor").classList.remove("fillcolor");
     // }    
-    if(document.querySelectorAll(".cf").length >0){
+    if (document.querySelectorAll(".cf").length > 0) {
       document.querySelector(".cf").classList.remove("cf");
-    }      
-    if(document.querySelectorAll("#"+stateattr).length >0){
-      document.querySelector("#"+stateattr).classList.add("cf"); 
     }
-}
-state_name
-tablerowHower(hoverValue){
-  
-  if(document.querySelectorAll(".fillcolor").length >0){
-    document.querySelector(".fillcolor").classList.remove("fillcolor");   
-  } 
-  if(document.querySelectorAll("#IN-"+hoverValue).length >0){
-    document.getElementById("IN-"+hoverValue).classList.add("fillcolor");  
-    this.state_name=document.getElementById("IN-"+hoverValue).getAttribute("title") 
-  }  
-  this.enter(hoverValue,this.state_name)
-} 
+    if (document.querySelectorAll("#" + stateattr).length > 0) {
+      document.querySelector("#" + stateattr).classList.add("cf");
+    }
+  }
+  state_name
+  tablerowHower(hoverValue) {
+
+    if (document.querySelectorAll(".fillcolor").length > 0) {
+      document.querySelector(".fillcolor").classList.remove("fillcolor");
+    }
+    if (document.querySelectorAll("#IN-" + hoverValue).length > 0) {
+      document.getElementById("IN-" + hoverValue).classList.add("fillcolor");
+      this.state_name = document.getElementById("IN-" + hoverValue).getAttribute("title")
+    }
+    this.enter(hoverValue, this.state_name)
+  }
   cancelModal() {
     Object.keys(this.modalObject).forEach(elem => {
       this.modalObject[elem] = false;
@@ -283,13 +283,13 @@ tablerowHower(hoverValue){
   // onChangeCountryDropDown(selectedValue){
   //   this.getmanageCountryInfo(selectedValue)
   // }
-  statedatainfo(){
+  statedatainfo() {
     this.ManageInfoService.statedatainfo(this.selectedStateAttr);
     this.router.navigateByUrl(`/state-info/${this.selectedStateAttr}`);
   }
 }
-export interface UserSelectedEventArgs{
-  selectedState:string
+export interface UserSelectedEventArgs {
+  selectedState: string
 }
 
 
